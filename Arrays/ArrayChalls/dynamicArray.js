b98c8110b78ea9b43c8368367d70e0fe080d3a34
unknown <rainwol@outlook.com>
2025-06-18 17:41:07 +1000

    solution to dynamic array resizing

--- Arrays/ArrayChalls/dynamicArray.js
+++ Arrays/ArrayChalls/dynamicArray.js
@@ -1,5 +1,18 @@
 // The challenge is to double an array n and double it k times then return the length of the new array
 
 function arrayDub(arr, n){
-	
+	this.capacity = arr.length;
+
+	for(let i = 0; i < n; i++){
+		this.capacity *= 2;
+	}
+
+	let newArr = Array(this.capacity);
+
+	for(let i = 0; i < arr.length; i++){
+		newArr[i] = arr[i];
+	}
+
+	return newArr.length;
+
 }
