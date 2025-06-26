a9376cb0e0676331948e8ed69e00c8231a6c3911
unknown <rainwol@outlook.com>
2025-06-26 19:29:54 +1000

    new solution added

--- Arrays/ArrayChalls/isValid.js
+++ Arrays/ArrayChalls/isValid.js
@@ -10,3 +10,34 @@ function isValid(s) {
 }
 
 console.log(isValid(x));
+
+
+let x = '()()';
+
+function isValid(s){
+    let stack = [];
+    let pairs = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    };
+
+    for(p of s){
+        if (p in pairs){
+            stack.push(p);
+            console.log(p);
+            console.log(stack);
+        } else {
+            if (stack.length === 0 || pairs[stack.pop()] !== p) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+
+}
+
+console.log(isValid(x));
+
+Solution to above On and On
